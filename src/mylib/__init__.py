e7c41ecd5c583e4961978fd7536d6e8ed2940e2a
Ruhong <seahrh@gmail.com>
2021-04-10 08:54:20 +0800

    get f1 score per row

--- src/mylib/__init__.py
+++ src/mylib/__init__.py
@@ -1,6 +1,15 @@
+import numpy as np
 import pandas as pd
 
 
 def target_label(df: pd.DataFrame) -> pd.Series:
     tmp = df.groupby("label_group")["posting_id"].unique().to_dict()
     return df["label_group"].map(tmp)
+
+
+def metric_per_row(prediction: str, target: str = "target"):
+    def f1(row):
+        n = len(np.intersect1d(row[target], row[prediction]))
+        return 2 * n / (len(row[target]) + len(row[prediction]))
+
+    return f1
