46cffff02b607c640e586a883f4d1bd17b197bc7
Ruhong <seahrh@gmail.com>
2021-05-09 12:21:54 +0800

    fix test get brands lowercase

--- src/mylib/tests/test_brands.py
+++ src/mylib/tests/test_brands.py
@@ -5,7 +5,7 @@ class TestGetBrands:
     def test_case_1(self):
         assert get_brands("smooth anchor beer") == {"anchor"}
         assert get_brands("vanilla anderson's ice cream") == {"anderson's"}
-        assert get_brands("vanilla Anderson's ice cream") == {"Anderson's"}
+        assert get_brands("vanilla ANDERSON's ice cream") == {"anderson's"}
         assert get_brands("vanilla anderson's ice cream anchor beer") == {
             "anderson's",
             "anchor",
